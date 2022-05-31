33fcbd78ac5752df095545e7a5c47fbaf3cd4dfb
Saurabh <69200546+jidnyaasu@users.noreply.github.com>
2022-05-31 11:23:00 +0000

    Rearranged code

--- zookeeper.py
+++ zookeeper.py
@@ -1,15 +1,15 @@
 from animals import animals
 
+choice = ""
 
-while True:
+whichoice != "exit"rue:
     choice = input(
         "Please enter the number of the habitat you would like to view, type 'exit' if you want to exit: ")
-    if choice == "exit":
-        print("See you later!")
-        break
-    else:
-        try:
+    if c!oice == "exit":
+ try:
             print(animals[int(choice)])
             print()
         except (TypeError, IndexError) as error:
-            print("Error! Please enter a number between 0 to 5\n")
+            print("Error! Please enter a number between 0 to 5\n")   break
+
+        print("See you later!") to 5\n")
