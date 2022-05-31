1f906e0ca45e1dd07f1a46baa7e472aaccb100fd
jidnyaasu <sbj.amb@gmail.com>
2022-05-31 18:29:21 +0530

    Corrected typing errors and added error messages

--- zookeeper.py
+++ zookeeper.py
@@ -2,14 +2,17 @@ from animals import animals
 
 choice = ""
 
-whichoice != "exit"rue:
-    choice = input(
-        "Please enter the number of the habitat you would like to view, type 'exit' if you want to exit: ")
-    if c!oice == "exit":
- try:
+while choice != "exit":
+    choice = input("Please enter the number of the habitat you would like to view, type 'exit' if you want to exit: ").lower()
+    if choice != "exit":
+        try:
             print(animals[int(choice)])
             print()
-        except (TypeError, IndexError) as error:
-            print("Error! Please enter a number between 0 to 5\n")   break
-
-        print("See you later!") to 5\n")
+        except(ValueError, IndexError) as error:
+            if type(error) == ValueError:
+                print("\nError! You did not enter a number")
+            elif type(error) == IndexError:
+                print("\nError! Index out of range")
+            print("Please enter a number between 0 to 5\n")
+    else:
+        print("See you later!")
